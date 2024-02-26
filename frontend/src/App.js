c3f2477c1e4725241329f1b01b02284589d70920
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2024-02-26 23:05:32 +0530

    Added Router to the DOM

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Components/Root/Root";
 
 function App() {
-  return <div className="App"></div>;
+  const router = createBrowserRouter([{ path: "/", element: <Root /> }]);
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
