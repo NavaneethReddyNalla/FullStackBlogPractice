a44bded84e9b9c8e9fd4d5bb1c49ab292de71938
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2024-02-26 23:42:03 +0530

    Created the Home, signin and signup outlets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./Components/Root/Root";
+import Home from "./Components/Screens/Home/Home";
+import SignIn from "./Components/Screens/SignIn/SignIn";
+import SignUp from "./Components/Screens/SignUp/SignUp";
 
 function App() {
-  const router = createBrowserRouter([{ path: "/", element: <Root /> }]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/signin", element: <SignIn /> },
+        { path: "/signup", element: <SignUp /> },
+      ],
+    },
+  ]);
   return (
     <div className="App">
       <RouterProvider router={router} />
