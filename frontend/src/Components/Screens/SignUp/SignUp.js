9a715fbe70598b18582ba885e5369bde282cb3ea
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2024-02-28 19:35:46 +0530

    Added two radio buttons in signup

--- frontend/src/Components/Screens/SignUp/SignUp.js
+++ frontend/src/Components/Screens/SignUp/SignUp.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
+import "./SignUp.css";
 
 function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   return (
-    <div className="bg-primary background">
-      <form></form>
+    <div className="bg-primary background pt-5">
+      <form>
+        <div>
+          <input type="radio" name="userType" id="user" />
+          <label htmlFor="user">User</label>
+
+          <input type="radio" name="userType" id="author" />
+          <label htmlFor="user">Author</label>
+        </div>
+      </form>
     </div>
   );
 }
