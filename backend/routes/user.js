c3d6c5fa32a62eabe8b389def98fde01f1000996
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2024-02-29 20:17:51 +0530

    Added View Articles to Users

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,10 +4,30 @@ const userApp = express.Router();
 const { createUserOrAuthor, userOrAuthorLogin } = require("./util");
 const expressAsyncHandler = require("express-async-handler");
 
+let usersCollection;
+let articlesCollection;
+
+userApp.use((req, res, next) => {
+  usersCollection = req.app.get("usersCollection");
+  articlesCollection = req.app.get("articlesCollection");
+  next();
+});
+
 // User Creation
 userApp.post("/user", expressAsyncHandler(createUserOrAuthor));
 
 // User Login
 userApp.post("/login", expressAsyncHandler(userOrAuthorLogin));
+
+// Get all articles
+userApp.get(
+  "/articles",
+  expressAsyncHandler(async (req, res) => {
+    const articlesList = await articlesCollection
+      .find({ status: true })
+      .toArray();
+    res.send({ message: "All Articles", payload: articlesList });
+  })
+);
 
 module.exports = userApp;
