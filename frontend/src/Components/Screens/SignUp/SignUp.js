9f923ac0062a8f0fa80965b6cf1cca097f3f460f
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2024-02-29 18:37:27 +0530

    Created the signup page

--- frontend/src/Components/Screens/SignUp/SignUp.js
+++ frontend/src/Components/Screens/SignUp/SignUp.js
@@ -10,14 +10,32 @@ function SignUp() {
     formState: { errors },
   } = useForm();
 
+  function onFormSubmit(data) {
+    console.log(data);
+  }
+
   return (
     <div className="bg-primary background pt-5">
-      <form className="px-5 py-2">
+      <form className="px-5 py-2" onSubmit={handleSubmit(onFormSubmit)}>
         <div className="py-2">
-          <input type="radio" name="userType" id="user" />
-          <label htmlFor="user">User</label>
+          <input
+            type="radio"
+            {...register("userType")}
+            id="user"
+            value="user"
+            className="mx-1"
+          />
+          <label htmlFor="user" className="me-3">
+            User
+          </label>
 
-          <input type="radio" name="userType" id="author" />
+          <input
+            type="radio"
+            {...register("userType")}
+            id="author"
+            value="author"
+            className="mx-1"
+          />
           <label htmlFor="user">Author</label>
         </div>
 
@@ -25,14 +43,14 @@ function SignUp() {
           type="text"
           className="form-control my-2"
           placeholder="Username"
-          name=""
+          {...register("username")}
           id="username"
         />
 
         <input
           type="password"
           placeholder="Password"
-          name=""
+          {...register("password")}
           id="password"
           className="form-control my-2"
         />
@@ -40,7 +58,7 @@ function SignUp() {
         <input
           type="email"
           placeholder="Email"
-          name=""
+          {...register("email")}
           id="email"
           className="form-control my-2"
         />
