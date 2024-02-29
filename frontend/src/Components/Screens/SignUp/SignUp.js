00688d68296deca4d1f1e778771eab4536a03b67
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2024-02-29 18:17:44 +0530

    Added all the buttons for signup

--- frontend/src/Components/Screens/SignUp/SignUp.js
+++ frontend/src/Components/Screens/SignUp/SignUp.js
@@ -12,14 +12,42 @@ function SignUp() {
 
   return (
     <div className="bg-primary background pt-5">
-      <form>
-        <div>
+      <form className="px-5 py-2">
+        <div className="py-2">
           <input type="radio" name="userType" id="user" />
           <label htmlFor="user">User</label>
 
           <input type="radio" name="userType" id="author" />
           <label htmlFor="user">Author</label>
         </div>
+
+        <input
+          type="text"
+          className="form-control my-2"
+          placeholder="Username"
+          name=""
+          id="username"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          name=""
+          id="password"
+          className="form-control my-2"
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+          name=""
+          id="email"
+          className="form-control my-2"
+        />
+
+        <button type="submit" className="btn btn-success my-2">
+          Register
+        </button>
       </form>
     </div>
   );
