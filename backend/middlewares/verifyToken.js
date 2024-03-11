0bb9e0690f3a10cb2d9eb7522f410bf67ad39314
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2024-03-11 12:06:42 +0530

    Completed the verifyToken Middleware

--- backend/middlewares/verifyToken.js
+++ backend/middlewares/verifyToken.js
@@ -1,1 +1,21 @@
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const verifyToken = (req, res, next) => {
+  const bearerToken = req.headers.authorization;
+
+  if (!bearerToken) {
+    res.send({ message: "No Token Found" });
+  }
+
+  const token = bearerToken.split(" ")[1];
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = verifyToken;
