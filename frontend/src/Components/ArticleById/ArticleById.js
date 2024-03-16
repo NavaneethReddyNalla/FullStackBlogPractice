c3543bc7be455d070e92c478bcd9d3dfe5387399
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2024-03-16 16:45:28 +0530

    Changed Article Background

--- frontend/src/Components/ArticleById/ArticleById.js
+++ frontend/src/Components/ArticleById/ArticleById.js
@@ -1,22 +1,34 @@
+import "./ArticleById.css";
+
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function ArticleById() {
   const article = useLocation().state;
+  const navigate = useNavigate();
+
+  function goBack() {
+    navigate("../articles");
+  }
 
   return (
-    <div className="article">
-      <h2>{article.title}</h2>
-      <h5>- By {article.username}</h5>
-      <h6>
-        <i>{article.category}</i>
-      </h6>
-      <h6>
-        <i>{article.dateOfModification}</i>
-      </h6>
-      <br />
-      <p>{article.content}</p>
-    </div>
+    <>
+      <button className="btn btn-danger ms-4 mt-5 back-button" onClick={goBack}>
+        Back
+      </button>
+      <div className="text-start m-auto w-50 p-4 bg-white article">
+        <h2>{article.title}</h2>
+        <h5>- By {article.username}</h5>
+        <h6>
+          <i>{article.category}</i>
+        </h6>
+        <h6>
+          <i>{article.dateOfModification}</i>
+        </h6>
+        <br />
+        <p>{article.content}</p>
+      </div>
+    </>
   );
 }
 
